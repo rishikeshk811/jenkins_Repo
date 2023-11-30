e1352dc53979502abe2f5fd5dc0af74dbdf00d42
rishikeshk811 <arprankmaster2020@gmail.com>
2023-11-30 16:04:58 +0530

    poll SCM

--- githubProject/src/main/java/com/OTM/GenericUtils/SampleTest.java
+++ githubProject/src/main/java/com/OTM/GenericUtils/SampleTest.java
@@ -1,19 +1,26 @@
 package com.OTM.GenericUtils;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class SampleTest {
 
-	WebDriver driver;
+	
 	
 	@Test
 	public void m1Test() {
-		driver=new ChromeDriver();
 		
-		driver.get("https://www.amazon.in");
-		System.out.println("browser is launching ");
 	
+		
+		
+		
+		
+		
+		
+		
+		System.out.println("m1Test is launching ");
+	
+		
+		
+		
 	}
 }
